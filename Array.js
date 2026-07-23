040e67303085868d372183e52e8d5d43029cd5e3
Muhammad Sami-Ud-Din <muhammadsmmdn2007@gmail.com>
2026-07-23 23:57:08 +0500

    for loop

--- Array.js
+++ Array.js
@@ -19,3 +19,21 @@ console.log(Object.keys(fruits));
 // 2. fruits.keys()
 console.log([...fruits.keys()]);
 // Output: [0, 1, 2, 3, 4, 5]    <-- Includes 3 and 4, returns numbers!
+
+
+
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+const iterator = colors.keys();
+for (const key of iterator) {
+  console.log(`${key}: ${colors[key]}`);
+}
+// Output
+// 0: red
+// 1: yellow
+// 2: blue
+// 3: undefined
+// 4: undefined
+// 5: purple
+
+const newColors = colors.toReversed(); // ['purple', undefined, undefined, 'blue', 'yellow', 'red']
